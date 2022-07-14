8ef68eadcc2aa7f96f454714320d21cfb19552b7
Roni Choudhury <roni.choudhury@kitware.com>
2022-07-14 12:31:08 -0400

    Add initial experimentation files

---
+++ index.js
@@ -0,0 +1,66 @@
+import './node_modules/geojs/geo.min.js';
+
+async function getNetwork() {
+    const resp = await fetch('network.dat');
+    const text = await resp.text();
+
+    let nodes = {};
+    let edges = [];
+
+    const addNode = (id) => {
+        if (!nodes.hasOwnProperty(id)) {
+            nodes[id] = {
+                id,
+            };
+        }
+    }
+
+    for (let line of text.split("\n")) {
+        if (line) {
+            const [source, target] = line
+                .split(" ")
+                .map((x) => parseInt(x));
+
+            // Add the nodes to the node list.
+            addNode(source);
+            addNode(target);
+
+            // Add the edge between them.
+            edges.push({
+                source,
+                target,
+            });
+        }
+    }
+
+    return {
+        nodes: Object.values(nodes),
+        edges,
+    }
+}
+
+const network = await getNetwork();
+console.log(network);
+
+const map = geo.map({
+    node: "#map",
+    center: {x: 0, y: 0},
+    zoom: 0,
+});
+
+const layer = map.createLayer('feature', {
+    features: ["point"],
+});
+
+const pointData = [
+    {x: 0, y: 0},
+];
+
+const point = layer.createFeature("point")
+    .data(pointData)
+    .style({
+        strokeColor: "black",
+        fillColor: "red",
+    });
+
+point.draw();
