8e07e54f569b02178c8b4f74fb2385ff5bf5958c
Roni Choudhury <roni.choudhury@kitware.com>
2022-10-17 11:38:12 -0400

    Add option to load network data

--- cines-react/src/App.tsx
+++ cines-react/src/App.tsx
@@ -1,16 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Graph from './Graph';
 import Toolbar from '@mui/material/Toolbar';
 import ToolbarMenu from './ToolbarMenu';
 import Typography from '@mui/material/Typography';
+import { GraphData, fetchNetworkData, getNetwork } from './util';
 import './App.css';
 
 function App() {
   const [nodeColor, setNodeColor] = useState("");
   const [edgeColor, setEdgeColor] = useState("");
   const [layout, setLayout] = useState("");
+  const [graphDataset, setGraphDataset] = useState("");
+  const [graphData, setGraphData] = useState<GraphData>({ nodes: [], edges: [] });
+
+  useEffect(() => {
+    const processData = async () => {
+      const networkData = await fetchNetworkData(graphDataset);
+      console.log(networkData);
+      setGraphData(getNetwork(networkData));
+    };
+
+    processData();
+  }, [graphDataset]);
+
+  useEffect(() => {
+    console.log(graphData);
+  }, [graphData]);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -19,6 +36,14 @@ function App() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
             CINES Playground
           </Typography>
+
+          <ToolbarMenu
+            header="Dataset"
+            description="Graph dataset"
+            options={["network.dat"]}
+            setExternalState={setGraphDataset}
+          />
+
           <ToolbarMenu
             header="Node color"
             description="Color for nodes"
